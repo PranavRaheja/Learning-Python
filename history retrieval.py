4ddf7d64d868945837ba1ea9603031fe23d110e8
Pranav Raheja <93196592+PranavRaheja@users.noreply.github.com>
2024-03-22 18:25:44 +0530

    Add files via upload
    This program helps you input your Google Chrome History file and retrieve data from all tables and store them individually in csv files

---
+++ history retrieval.py
@@ -0,0 +1,47 @@
+import sqlite3
+import pandas as pd
+import time
+
+try:
+    # Connect to the SQLite database
+    conn = sqlite3.connect('C:\\Users\\iampr\\Desktop\\history python\\hist_chrome')
+    
+    # if connected
+    print("Connection to SQLite database successful!")
+except sqlite3.Error as e:
+    
+    
+    # if not connected
+    print("Error connecting to SQLite database:", e)
+ 
+#get table names   
+def get_table_names(conn):
+    cur = conn.cursor()
+    query = """SELECT name FROM sqlite_master WHERE type='table';"""
+    cur.execute(query)
+    fetch = cur.fetchall()
+#make a list of just the string(table name) and get rid of commas and other characters
+    tables = [table[0] for table in fetch] 
+#print them invidually
+    print("All the table names are:\n")
+    
+    for i in range(0,18):
+        print ("Table ",[i+1],":",tables[i])
+    print("\033[34;5mCreating files...\033[0m")
+
+    time.sleep(2)
+    print("\033[32;5mCreated :)\033[0m")
+    for table in tables:
+        cur.execute(f"""SELECT * FROM {table}""")
+        data = cur.fetchall() 
+        col_name = [desc[0]for desc in cur.description]
+        df = pd.DataFrame(data, columns=col_name)
+        df.to_csv(f'C:\\Users\\iampr\\Desktop\\python project\\CSV FILES\\{table}.csv',index=False)
+
+get_table_names(conn)
+
+
+
+
+
+
